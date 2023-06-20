a0e0aba519d75b5fcca304a5c7de376a8ffa3cff
Sunny <tjsghktjswls@naver.com>
2023-06-21 00:54:52 +0900

    Update 단어 공부.swift

--- 백준/Bronze/1157. 단어 공부/단어 공부.swift
+++ 백준/Bronze/1157. 단어 공부/단어 공부.swift
@@ -1,3 +1,30 @@
+// 1번째 방법
+func solution1152() {
+    
+    let input = Array(readLine()!)
+    
+    var dictionay: [String: Int] = [:]
+    input.forEach { value in
+        let uppercasedString = value.uppercased()
+        dictionay[String(uppercasedString), default: 0] += 1
+    }
+    
+    var maxCount = 0
+    var maxString = ""
+    for (key, value) in dictionay {
+        if maxCount < value {
+            maxString = key
+            maxCount = value
+        } else if maxCount == value {
+            maxString += key
+        }
+    }
+    print(maxString.count > 1 ? "?" : "\(maxString)")
+}
+
+solution1152()
+
+// 2번째 방법
 func solution1157() {
     
     let input = readLine()!.uppercased()

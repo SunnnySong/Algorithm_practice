70318cbfce9f4428a8fd5d81b2705bf0c5c6b654
Sunny <tjsghktjswls@naver.com>
2023-06-21 22:10:40 +0900

    [Silver V] Title: 너의 평점은, Time: 16 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.swift
@@ -0,0 +1,56 @@
+import Foundation
+
+enum Grade: String {
+    case plusA = "A+"
+    case zeroA = "A0"
+    case plusB = "B+"
+    case zeroB = "B0"
+    case plusC = "C+"
+    case zeroC = "C0"
+    case plusD = "D+"
+    case zeroD = "D0"
+    case F = "F"
+    
+    var score: Double {
+        switch self {
+        case .plusA:
+            return 4.5
+        case .zeroA:
+            return 4.0
+        case .plusB:
+            return 3.5
+        case .zeroB:
+            return 3.0
+        case .plusC:
+            return 2.5
+        case .zeroC:
+            return 2.0
+        case .plusD:
+            return 1.5
+        case .zeroD:
+            return 1.0
+        case .F:
+            return 0
+        }
+    }
+}
+
+func solution2941() {
+    
+    var total: Double = 0
+    var totalScore: Double = 0
+    
+    for _ in 0..<20 {
+        let input = readLine()!.components(separatedBy: " ")
+        let score = Double(input[1])!
+        guard let grade = Grade(rawValue: input[2]) else {
+            continue
+        }
+        
+        total += score * grade.score
+        totalScore += score
+    }
+    print(total / totalScore)
+}
+
+solution2941()

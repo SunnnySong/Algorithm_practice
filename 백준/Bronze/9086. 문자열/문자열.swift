049572ffc267bdbb646e1abce73365006c7b64ca
Sunny <tjsghktjswls@naver.com>
2023-07-07 00:36:46 +0900

    [Bronze V] Title: 문자열, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.swift
@@ -0,0 +1,9 @@
+func solution() {
+    let testCase = Int(readLine()!)!
+    for _ in 0..<testCase {
+        let input = readLine()!
+        print("\(input.first!)\(input.last!)")
+    }
+}
+
+solution()

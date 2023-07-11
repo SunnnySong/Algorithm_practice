c84cc359776f049e16e0a8a8b1cab7de6c6280bd
Sunny <tjsghktjswls@naver.com>
2023-07-11 11:17:44 +0900

    Update 네 번째 점.swift

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.swift
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.swift
@@ -1,3 +1,4 @@
+// ver 1.
 func solution() {
     var x = Set<Int>()
     var y = Set<Int>()
@@ -22,3 +23,21 @@ func solution() {
 }
 
 solution()
+
+// ver2.
+func solution() {
+    
+    var x = 0, y = 0
+    for _ in 0..<3 {
+        let input = readLine()!.split(separator: " ").map { Int($0)! }
+        x ^= input[0]
+        y ^= input[1]
+        print(x, y)
+    }
+}
+
+- XOR 연산은 두 개의 비트가 다를 때 결과가 1이 되고, 같을 때 결과가 0이 된다.
+- XOR 연산은 여러 개의 비트를 XOR 연산할 때, 짝이 없는 유일한 비트를 찾는 데에 유용하다.
+- ex)(1, 1), (1, 3), (3, 1) 세 개의 좌표가 주어진 경우, 
+    - x값들의 XOR 연산: 1 XOR 1 XOR 3 = 3
+    - y값들의 XOR 연산: 1 XOR 3 XOR 1 = 3

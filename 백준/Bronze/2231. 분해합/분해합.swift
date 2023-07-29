c8da08d845a02ccfdfa33bddec5faf4f6e69528c
Sunny <tjsghktjswls@naver.com>
2023-07-29 18:12:46 +0900

    [Bronze II] Title: 분해합, Time: 304 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.swift
+++ 백준/Bronze/2231. 분해합/분해합.swift
@@ -1,28 +1,22 @@
 func solution() {
     let input = Int(readLine()!)!
     var sum = 0
+    var m = 0
 
     for i in 1...input {
-        let number = Array(String(i))
         sum = i
 
-        for h in 0..<number.count {
-            sum += Int(String(number[h]))!
-
-            if sum > input {
-                break
-            }
+        for h in String(i) {
+            sum += Int(String(h))!
         }
 
         if sum == input {
-            print(i)
+            m = i
             break
         }
     }
 
-    if sum != input {
-        print(0)
-    }
+    print(m)
 }
 
 solution()

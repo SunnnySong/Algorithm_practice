a57b3baf308b232224ea50e2dcee4af6c69b7fea
Sunny <tjsghktjswls@naver.com>
2023-08-12 00:57:47 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1364 ms, Memory: 89152 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.swift
@@ -0,0 +1,13 @@
+func solution() {
+    let count = Int(readLine()!)!
+    var array = [Int]()
+
+    for _ in 0..<count {
+        let number = Int(readLine()!)!
+        array.append(number)
+    }
+
+    array.sorted().forEach { print($0, terminator: "\n") }
+}
+
+solution()

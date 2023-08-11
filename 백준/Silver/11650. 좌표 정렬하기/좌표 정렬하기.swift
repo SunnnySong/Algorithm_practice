2f7f54d1132fe728326e9876d4e104b32b4798fe
Sunny <tjsghktjswls@naver.com>
2023-08-12 02:41:02 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 260 ms, Memory: 77600 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.swift
@@ -0,0 +1,20 @@
+func solution() {
+    let count = Int(readLine()!)!
+    var array = [[Int]]()
+
+    for _ in 0..<count {
+        let input = readLine()!.split(separator: " ").map { Int($0)! }
+        array.append(input)
+    }
+
+    let sorted = array.sorted { pre, next in
+        if pre[0] == next[0] {
+            return pre[1] < next[1] ? true : false
+        } else {
+            return pre[0] < next[0] ? true : false
+        }
+    }
+    sorted.forEach { print($0.map { String($0 )}.joined(separator: " "), terminator: "\n")}
+}
+
+solution()

da491358bd9752530d62afcd2919320321e144c8
Sunny <tjsghktjswls@naver.com>
2023-08-23 16:39:00 +0900

    [Silver V] Title: 최소공배수, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.swift
@@ -0,0 +1,20 @@
+func solution() {
+    let numbers = readLine()!.split(separator: " ").map { Int($0)! }
+    let a = numbers[0], b = numbers[1]
+
+    print(최소공배수(a, b))
+}
+
+func 최소공배수(_ a: Int, _ b: Int) -> Int {
+    return a * b / 최대공약수(a, b)
+}
+
+func 최대공약수(_ a: Int, _ b: Int) -> Int {
+    if b == 0 {
+        return a
+    } else {
+        return 최대공약수(b, a % b)
+    }
+}
+
+solution()
